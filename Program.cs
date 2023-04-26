03132e44323229df95d1bcb417184307881c8d86
Bruno Lourenço <brunolourencodias@hotmail.com.br>
2023-04-26 13:23:45 +0044

    remove swagger

--- Program.cs
+++ Program.cs
@@ -26,12 +26,10 @@ namespace CurrencyQuotationMinimal.Api
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+          
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+          
 
             app.UseHttpsRedirection();
 
